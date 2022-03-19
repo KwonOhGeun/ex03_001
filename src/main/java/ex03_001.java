682e80bcec301e29b2cd6826903df1990a839ac1
KwonOhGeun <ohgeun72@naver.com>
2022-03-19 21:42:43 +0900

    Initial commit

---
+++ src/main/java/ex03_001.java
@@ -0,0 +1,12 @@
+public class ex03_001 {
+
+    public static void main(String[] args) {
+
+      int a = 100;
+      int b = 200;
+
+        System.out.printf("%d\n", a+b);
+        System.out.printf("%.1f", (float)a/b);
+
+    }
+}
